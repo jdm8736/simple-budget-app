34194ca829e0055d3edc20df4d1167996f3748ad
Josh Masterson <joshua.masterson@viasat.com>
2017-08-12 20:03:15 -0500

    test docker update

--- src/app/accounts/components/AccountsList.vue
+++ src/app/accounts/components/AccountsList.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="accounts-list-view">
-    I'm a list of accounts!
+    I'm a list of accounts OR AM I!
 
     <router-link :to="{ name: 'createAccount' }">Add an Account</router-link>
     <router-link :to="{ name: 'budgetsList' }">View budgets</router-link>
